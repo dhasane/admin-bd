e028786b530f45f6dd7a1acd42042cae05eb2696
dhasane <danihas@live.com>
2021-03-20 01:45:50 -0500

    Log out button

--- index.php
+++ index.php
@@ -3,10 +3,8 @@ session_start();
 //var_dump($_SESSION);
 
 
-if(!isset($_SESSION['login'])) {
+if (!isset($_SESSION['login'])) {
     header("Location: ./login.php");
-} else {
-    $GLOBAL['conexion'] = $_SESSION['login'];
 }
 
 ?>
@@ -16,21 +14,24 @@ if(!isset($_SESSION['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
+
     <link href='https://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Chelsea+Market&display=swap" rel="stylesheet">
-    
 
+    <title>Inicio</title>
 
-    <title>Inicio</title> 
-    
 </head>
 
 <body>
     <div class="titulo_pagina">
         <h1>Sistema de administración de bases de datos</h1>
     </div>
+
+    <form method="post">
+        <input type="submit" name="log out" id="logoutbtn" value="RUN" /><br />
+    </form>
+
 
     <table border='1' style="width:100%">
         <caption>Lista de usuarios disponibles</caption>
@@ -45,13 +46,21 @@ if(!isset($_SESSION['login'])) {
             <?php
             include_once 'consultas.php';
             include_once 'configuracion.php';
-            $usuarios = lista_Usuarios();
-            while ($fila = oci_fetch_array($usuarios, OCI_ASSOC+OCI_RETURN_NULLS)) 
+
+            function logout()
             {
+                unset($_SESSION);
+            }
+
+            if (array_key_exists('logoutbtn', $_POST)) {
+                logout();
+            }
+            $usuarios = lista_Usuarios();
+            while ($fila = oci_fetch_array($usuarios, OCI_ASSOC + OCI_RETURN_NULLS)) {
                 echo "<tr>\n";
-                    echo "<td>".$fila['USER_ID']."</td>";
-                    echo "<td>".$fila['USERNAME']."</td>";
-                    echo "<td><a href='pantallas/pantallaInicio.php?idUsuario=".$fila['USER_ID']."&nombreUsuario=".$fila['USERNAME']."'>seleccionar</td>\n";
+                echo "<td>" . $fila['USER_ID'] . "</td>";
+                echo "<td>" . $fila['USERNAME'] . "</td>";
+                echo "<td><a href='pantallas/pantallaInicio.php?idUsuario=" . $fila['USER_ID'] . "&nombreUsuario=" . $fila['USERNAME'] . "'>seleccionar</td>\n";
                 echo "</tr>\n";
             }
             ?>
