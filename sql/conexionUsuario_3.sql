ed1508b8e32693db4a193e8bb3b9e00add4f370e
proxar <pedroguerrero902@gmail.com>
2021-03-19 22:38:10 -0500

    inicio del poder de dba 2.0

--- sql/conexionUsuario_3.sql
+++ sql/conexionUsuario_3.sql
@@ -1,81 +1,15 @@
-DROP TABLE PRIMERATABLAUSUARIO_3;
-DROP TABLE SEGUNDATABLAUSUARIO_3;
-DROP TABLE TERCERATABLAUSUARIO_3;
-DROP TABLE CUARTATABLAUSUARIO_3;
+SELECT *
+FROM SYS.vista_todas_las_tablas;
 
-create table primeraTablaUsuario_3
+select *
+from sys.espacio_usuario;
+
+
+create table poder_1
 (
-     id number primary key,
-    cedula number NOT NULL,
-    nombre varchar2(100)
-) tablespace tablasProyecto;
+    id number primary key,
+    nombre varchar2(100) not null,
+    cedula number
+);
 
-create table segundaTablaUsuario_3
-(
-     id number primary key,
-    cedula number NOT NULL,
-    nombre varchar2(100)
-) tablespace tablasProyecto;
-
-
-create table terceraTablaUsuario_3
-(
-     id number primary key,
-    cedula number NOT NULL,
-    nombre varchar2(100)
-) tablespace tablasProyecto;
-
-create table cuartaTablaUsuario_3
-(
-     id number primary key,
-    cedula number NOT NULL,
-    nombre varchar2(100)
-) tablespace tablasProyecto;
-
-BEGIN
-DBMS_SCHEDULER.DROP_JOB('MI_PRIMER_JOB_USUARIO_3');
-END;
-/
-
-BEGIN
-DBMS_SCHEDULER.DROP_JOB('MI_SEGUNDO_JOB_USUARIO_3');
-END;
-/
-
-BEGIN
-DBMS_SCHEDULER.DROP_JOB('MI_TERCERO_JOB_USUARIO_3');
-END;
-/
-
-BEGIN
-  DBMS_SCHEDULER.create_job (
-    job_name        => 'MI_PRIMER_JOB_USUARIO_3',
-    job_type        => 'PLSQL_BLOCK',
-    job_action      => 'BEGIN refrescavista; END;',
-    start_date      => SYSTIMESTAMP,
-    repeat_interval => 'freq=hourly; byminute=0; bysecond=0;',
-    enabled         => TRUE);
-END;
-/
-
-BEGIN
-  DBMS_SCHEDULER.create_job (
-    job_name        => 'MI_SEGUNDO_JOB_USUARIO_3',
-    job_type        => 'PLSQL_BLOCK',
-    job_action      => 'BEGIN refrescavista; END;',
-    start_date      => SYSTIMESTAMP,
-    repeat_interval => 'freq=hourly; byminute=0; bysecond=0;',
-    enabled         => TRUE);
-END;
-/
-
-BEGIN
-  DBMS_SCHEDULER.create_job (
-    job_name        => 'MI_TERCERO_JOB_USUARIO_3',
-    job_type        => 'PLSQL_BLOCK',
-    job_action      => 'BEGIN refrescavista; END;',
-    start_date      => SYSTIMESTAMP,
-    repeat_interval => 'freq=hourly; byminute=0; bysecond=0;',
-    enabled         => TRUE);
-END;
-/
+insert into poder_1 values (1, 'pedro', 123554);
